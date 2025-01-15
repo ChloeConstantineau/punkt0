9838e4abc371cffc9d8155b52f08eabadacb7f92
Chloe Constantineau <chloe_constantineau@hotmail.com>
2025-01-15 11:37:56 -0500

    Refactor Symbols, add methods and GlobalScope

--- src/main/scala/io/punkt0/analyzer/Symbols.scala
+++ src/main/scala/io/punkt0/analyzer/Symbols.scala
@@ -10,12 +10,35 @@ object Symbols:
 
     abstract class Symbol extends PositionT
 
-    case class ClassSymbol(name: String) extends Symbol
+    case class GlobalScope(mainClass: ClassSymbol, classes: Map[String, ClassSymbol] = Map.empty):
+        def lookupClass(n: String): Option[ClassSymbol] = classes.get(n)
+
+    case class ClassSymbol(
+        name: String,
+        parent: Option[ClassSymbol] = None,
+        methods: Map[String, MethodSymbol] = Map.empty,
+        members: Map[String, VariableSymbol] = Map.empty,
+    ) extends Symbol:
+        private def lookupMethod(n: String): Option[MethodSymbol] =
+          methods.get(n).orElse(lookupMethodInParent(n))
+        private def lookupVar(n: String): Option[VariableSymbol]  =
+          members.get(n).orElse(lookupVarInParent(n))
+
+        // TODO Test if it recurses as expected
+        private def lookupMethodInParent(n: String): Option[MethodSymbol] =
+          parent.fold(None)(p => p.lookupMethod(n))
+        private def lookupVarInParent(n: String): Option[VariableSymbol]  =
+          parent.fold(None)(p => p.lookupVar(n))
 
     case class MethodSymbol(
         name: String,
-        classSymbol: ClassSymbol,
+        `class`: ClassSymbol,
         tpe: Type,
-    ) extends Symbol
+        params: Map[String, VariableSymbol] = Map.empty,
+        members: Map[String, VariableSymbol] = Map.empty,
+        argTpeList: List[Type] = Nil,
+        overridden: Option[MethodSymbol] = None,
+    ) extends Symbol:
+        def lookupVar(n: String): Option[VariableSymbol] = params.get(n).orElse(members.get(n))
 
     case class VariableSymbol(name: String, tpe: Type) extends Symbol
