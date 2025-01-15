24d153ea27dbfa353efd0256b07f05b731d8c199
Chloe Constantineau <chloe_constantineau@hotmail.com>
2025-01-15 00:07:30 -0500

    Added invalid test programs to Parser suite

--- src/test/scala/io/punkt0/ParserSpec.scala
+++ src/test/scala/io/punkt0/ParserSpec.scala
@@ -3,22 +3,23 @@ package io.punkt0
 import io.punkt0.ast.Parser
 import io.punkt0.lexer.Lexer
 import org.scalatest.matchers.should.Matchers.*
-import org.scalatest.wordspec.AnyWordSpecLike
 
 import java.io.File
 import scala.io.Source
 
-class ParserSpec extends AnyWordSpecLike:
+class ParserSpec extends UnitTest:
 
-    private val directory  = new File("./test-programs/ast/valid/")
-    private val readFiles  = (suffix: String) => directory.listFiles.filter(_.getName.endsWith(suffix))
+    private val directory  = (folder: String) => new File(s"./test-programs/ast/$folder/")
+    private val readFiles  = (folder: String, suffix: String) =>
+      directory(folder).listFiles.filter(_.getName.endsWith(suffix))
     private val computeAST = (file: File) =>
       (Lexer andThen Parser).run(file)(Context(file = Some(file))).toString
 
     "Parser" should:
-        val contentFiles = readFiles(".p0").sorted
-        val assertFiles  = readFiles(".p0.ast").sorted
+        val contentFiles = readFiles("valid", ".p0").sorted
+        val assertFiles  = readFiles("valid", ".p0.ast").sorted
 
+        // Valid test programs
         contentFiles.zip(assertFiles).foreach { file =>
           s"generate AST for ${file._1.getName}" in:
               val AST            = computeAST(file._1)
@@ -27,4 +28,10 @@ class ParserSpec extends AnyWordSpecLike:
               sourceExpected.close()
 
               assert(AST == expected)
+        }
+
+        // Invalid test programs
+        readFiles("invalid", ".p0").foreach { file =>
+          s"fail for ${file.getName}" in:
+              assertThrows[Throwable](computeAST(file))
         }
